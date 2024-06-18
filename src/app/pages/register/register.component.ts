521c10f0cd9a4901a39f9afcfc3079e9adeaa7f1
George Kalandadze <kalandadzeg777@gmail.com>
2024-06-18 18:48:58 +0400

    feat/make routing between login and register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,12 @@
 import { NgIf } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
 import { NgForm, FormsModule, NgModel } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule, NgIf],
+  imports: [FormsModule, NgIf, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
